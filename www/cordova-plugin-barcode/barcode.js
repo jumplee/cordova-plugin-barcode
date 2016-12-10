0948af6514b349fb7ff7e2190ef15525554f37a8
jumplee <liyuntao09@gmail.com>
2016-12-10 15:28:53 +0800

    去除外面的define否则无法获取js对象

--- www/cordova-plugin-barcode/barcode.js
+++ www/cordova-plugin-barcode/barcode.js
@@ -1,8 +1,6 @@
-cordova.define("cordova-plugin-barcode", function(require, exports, module) {
-    var exec = require("cordova/exec");
-    var BarcodeScan = function() {};
-    BarcodeScan.prototype.scan = function(successCallback, errorCallback) {
-        exec(successCallback, errorCallback, "BarcodeScan", "scan", []);
-    }
-    module.exports = new BarcodeScan();
-});
+var exec = require("cordova/exec");
+var BarcodeScan = function() {};
+BarcodeScan.prototype.scan = function(successCallback, errorCallback) {
+    exec(successCallback, errorCallback, "BarcodeScan", "scan", []);
+}
+module.exports = new BarcodeScan();
